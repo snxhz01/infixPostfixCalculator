a997b5eb6cd7d076072e8f3d46efa596c32e528e
snxhz01 <77512847+snxhz01@users.noreply.github.com>
2021-02-20 17:19:41 -0500

    New methods using class example
    have yet to make second stack

--- src/ConvertingStack.java
+++ src/ConvertingStack.java
@@ -1,27 +1,50 @@
 public class ConvertingStack {
 
-    private String[] convert;
-    private char mHead;
-    private int length;
+    private Number[] convertArr;
+    private int mHead;
+   // private int size;
 
-    public ConvertingStack(){
-        length = -1;
-        convert = new String[];
+
+    public ConvertingStack(int size) {
+        convertArr = new Number[size];
+       // size = (convertArr.length - 1);
+        mHead = 0;
     }
 
-    public boolean isEmpty(){
-        if(mHead.next == null){
+    public boolean isEmpty() {
+        if (convertArr[mHead] == null) {
             return true;
-        } else{
+        } else {
             return false;
         }
     }
 
-    public void push(){
+    public void push(Number element) {
+        if (mHead == convertArr.length - 1)
+            throw new IndexOutOfBoundsException();
 
+        mHead++;
+        convertArr[mHead] = element;
     }
 
-    public pop(){
+    public Number pop() {
+        if (isEmpty())
+            throw new IndexOutOfBoundsException();
 
+        Number element = (Number) convertArr[mHead];
+        mHead--;
+        return element;
     }
+
+    public Number peek() {
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (isEmpty() == false) {
+            return (Number) convertArr[mHead];
+        }
+
+        return null;
+    }
+
 }
